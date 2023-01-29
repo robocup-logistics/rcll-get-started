324b84d03356f9bc38847c71afa96162bcbfe769
Peter Kohout <peter.kohout@knapp.com>
2023-01-29 16:19:55 +0100

    config files for refbox now configureable.

--- setup.sh
+++ setup.sh
@@ -12,6 +12,11 @@ export SIMULATOR_IMAGE=quay.io/robocup-logistics/rcll-simulator
 export SIMULATOR_FRONTEND_IMAGE=quay.io/robocup-logistics/rcll-simulator-frontend
 
 export SIMULATOR_CONFIG_FILE=./../config/simulator/config.yaml
+export REFBOX_CONFIG_GAME=./../config/refbox/game.yaml
+export REFBOX_CONFIG_SIMULATION=./../config/refbox/simulation.yaml
+export REFBOX_CONFIG_COMM=./../config/refbox/comm.yaml
+export REFBOX_CONFIG_MPS=./../config/refbox/mps.yaml
+export REFBOX_CONFIG_TEAM=./../config/refbox/team.yaml
 
 function rc_start_refbox() {
   cd $rcll_get_started_dir/compose_files
