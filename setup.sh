b471b46c4a2d1446e6fcbd02a287ed6b37f8b672
Peter Kohout <peter.kohout@knapp.com>
2023-01-29 15:56:51 +0100

    finalized scripts to start refbox.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 rcll_get_started_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+export REFBOX_TAG=refactored
+export REFBOX_FRONTEND_TAG=v1
+
+export REFBOX_IMAGE=quay.io/robocup-logistics/rcll-refbox
+export REFBOX_FRONTEND_IMAGE=quay.io/robocup-logistics/rcll-sim-frontend
 function rc_start_refbox() {
   cd $rcll_get_started_dir/compose_files
   docker-compose -f refbox.yaml up
