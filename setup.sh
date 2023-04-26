d1e8e2c3e86f4dbed84785315fbb3f106c832712
Peter Kohout <peter.kohout@knapp.com>
2023-04-26 13:56:22 +0200

    added commands to export/import game reports.

--- setup.sh
+++ setup.sh
@@ -29,6 +29,21 @@ function rc_start_simulator() {
   docker-compose -f simulation.yaml up
 }
 
+function rc_dump_game_reports() {
+  date=$(date +"%Y-%m-%d_%T")
+  command=$(echo mongodump -d rcll -c game_report --gzip --archive=/data/db/game_report_$date.gz)
+  docker exec mongodb $command
+  mv $rcll_get_started_dir/compose_files/data/game_report_$date.gz game_report_$date.gz
+}
+
+function rc_restore_game_reports() {
+  date=$(date +"%Y-%m-%d_%T")
+  cp $1 $rcll_get_started_dir/compose_files/data/TMP_$date.gz
+  command=$(echo mongorestore --gzip --archive=/data/db/TMP_$date.gz)
+  docker exec mongodb $command
+  rm $rcll_get_started_dir/compose_files/data/TMP_$date.gz
+}
+
 function rc() {
  cd $rcll_get_started_dir
 }
@@ -37,6 +52,12 @@ LOCAL_SETUP=$rcll_get_started_dir/local_setup.sh
 if [ ! -f "$LOCAL_SETUP" ]; then
   echo "local settings [$LOCAL_SETUP] DOES NOT exist, creating empty file!"
   echo "" > $LOCAL_SETUP
+fi
+
+DATA_FOLDER=$rcll_get_started_dir/compose_files/data
+if [ ! -d "$DATA_FOLDER" ]; then
+  echo "data mount for mongodb [$DATA_FOLDER] DOES NOT exist, creating empty file!"
+  mkdir -p $DATA_FOLDER
 fi
 source $LOCAL_SETUP
 
